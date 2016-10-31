317b19b75f0de15f4ccb8218654cb4608be1bdb7
Cecilia de la Paz Villarreal Cavazos <ceciliadelapazvillarrealcavazos@Cecilias-MacBook-Pro.local>
2016-10-30 20:39:11 -0600

    Initial Commit

---
+++ ReproductorMusica/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReproductorMusica
+//
+//  Created by Cecilia de la Paz Villarreal Cavazos on 10/30/16.
+//  Copyright © 2016 Cecilia de la Paz Villarreal Cavazos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
